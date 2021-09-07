ac3b66ebb18afd7cfe24b0319ed79c7c0a9c3bcb
Rex4r <rex4r@inbox.ru>
2021-09-07 16:28:27 +0700

    button animate

--- tv/js/scripts.js
+++ tv/js/scripts.js
@@ -1,48 +1,64 @@
 let circle = document.getElementById('circle');
+let button = document.getElementById('button');
 let link = document.getElementById('link');
+let linkText = document.getElementById('link-text');
 
-circle.onmousedown = function(event) {
+function start(event) {
+console.log(event.type)
   event.stopPropagation();
   event.preventDefault();
 
-  let startX = 365;
-  let endX = -10;
-  let startCursorX = event.pageX
+  circle.style.marginLeft = -circle.offsetWidth / 2 + 'px';
+
+  let maxX = button.offsetWidth;
+  let endX = 0;
+  let startX = circle.offsetLeft - (-circle.offsetWidth / 2);
+  let startCursorX = event.clientX || event.touches[0].pageX
 
   function moveTo(diffX) {
     let currentX = startX - diffX;
 
     if (currentX < endX) {
       currentX = endX;
-    } else if (currentX > startX) {
-      currentX = startX;
+    } else if (currentX > maxX) {
+      currentX = maxX;
     }
 
     circle.style.left = currentX + 'px';
+    linkText.style.opacity = currentX/maxX;
 
     if (currentX === endX) {
-      window.location.href = link.href;
+      //window.location.href = link.href;
     }
   }
 
-  document.onmousemove = function(event) {
-    let diffX = startCursorX - event.pageX;
+  function move(event) {
+    let currentCursorX = event.clientX || event.touches[0].pageX;
+    let diffX = startCursorX - currentCursorX;
 
     moveTo(diffX);
   }
+  document.ontouchmove = move;
+  document.onmousemove = move;
 
-  document.onmouseup = function() {
-    document.onmousemove = null;
-    circle.onmouseup = null;
+  function end() {
+    document.ontouchmove = document.onmousemove = null;
+    circle.ontouchend = circle.onmouseup = null;
   }
+
+  document.ontouchend = end;
+  document.onmouseup = end;
 }
+
+circle.ontouchstart = start;
+circle.onmousedown = start;
 
 link.onclick = function(event) {
   event.stopPropagation();
   event.preventDefault();
 
-  circle.classList.add("button__circle--animation");
+  button.classList.add("button--animate");
   setTimeout(function() {
-    window.location.href = link.href;
+    // window.location.href = link.href;
   }, 1200);
 }
